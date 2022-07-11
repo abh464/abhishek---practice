74d21e7fc462d9ad792c0992671270cc07c85388
abh464 <abhishekusha546@gmail.com>
2022-07-11 22:04:42 +0530

    new Changes

--- TrappingRainWater.java
+++ TrappingRainWater.java
@@ -36,7 +36,8 @@ public static void main(String args[]){
 
     
     List<Integer> height = new ArrayList<Integer>(Arrays.asList(0 , 1, 0 , 2, 1 , 0 , 1 ,3, 2 ,1 , 2 ,1));
-
+    
+    // Input array is given here
     System.out.print("Total water Trapped will be : "+Trapping_Rain_Water(height));
 }
 }
